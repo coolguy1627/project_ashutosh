65c1c25e774932b7806e9423e7b25827abc4cce9
Ashutosh Kumar <mrashutosh1501@gmail.com>
2022-06-15 18:33:31 +0530

    ashutosh

---
+++ OnlineStore/Migrations/20220615113627_create-database.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineStore.Migrations
+{
+    public partial class createdatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 515, DateTimeKind.Local).AddTicks(3122));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 515, DateTimeKind.Local).AddTicks(4341));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 515, DateTimeKind.Local).AddTicks(4397));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 515, DateTimeKind.Local).AddTicks(4406));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 515, DateTimeKind.Local).AddTicks(4410));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 507, DateTimeKind.Local).AddTicks(4086));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 514, DateTimeKind.Local).AddTicks(7571));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 514, DateTimeKind.Local).AddTicks(8213));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTime",
+                value: new DateTime(2020, 1, 25, 17, 32, 7, 932, DateTimeKind.Local).AddTicks(6862));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationTime",
+                value: new DateTime(2020, 1, 25, 17, 32, 7, 932, DateTimeKind.Local).AddTicks(8682));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationTime",
+                value: new DateTime(2020, 1, 25, 17, 32, 7, 932, DateTimeKind.Local).AddTicks(8728));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreationTime",
+                value: new DateTime(2020, 1, 25, 17, 32, 7, 932, DateTimeKind.Local).AddTicks(8736));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreationTime",
+                value: new DateTime(2020, 1, 25, 17, 32, 7, 932, DateTimeKind.Local).AddTicks(8741));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTime",
+                value: new DateTime(2020, 1, 25, 17, 32, 7, 914, DateTimeKind.Local).AddTicks(8136));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationTime",
+                value: new DateTime(2020, 1, 25, 17, 32, 7, 931, DateTimeKind.Local).AddTicks(6309));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationTime",
+                value: new DateTime(2020, 1, 25, 17, 32, 7, 931, DateTimeKind.Local).AddTicks(7193));
+        }
+    }
+}
---
+++ OnlineStore/Migrations/20220615114051_createdb.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineStore.Migrations
+{
+    public partial class createdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 10, 50, 972, DateTimeKind.Local).AddTicks(6659));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 10, 50, 972, DateTimeKind.Local).AddTicks(7681));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 10, 50, 972, DateTimeKind.Local).AddTicks(7715));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 10, 50, 972, DateTimeKind.Local).AddTicks(7718));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 10, 50, 972, DateTimeKind.Local).AddTicks(7721));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 10, 50, 964, DateTimeKind.Local).AddTicks(5027));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 10, 50, 972, DateTimeKind.Local).AddTicks(2212));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 10, 50, 972, DateTimeKind.Local).AddTicks(2846));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 515, DateTimeKind.Local).AddTicks(3122));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 515, DateTimeKind.Local).AddTicks(4341));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 515, DateTimeKind.Local).AddTicks(4397));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 515, DateTimeKind.Local).AddTicks(4406));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 515, DateTimeKind.Local).AddTicks(4410));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 507, DateTimeKind.Local).AddTicks(4086));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 514, DateTimeKind.Local).AddTicks(7571));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationTime",
+                value: new DateTime(2022, 6, 15, 17, 6, 26, 514, DateTimeKind.Local).AddTicks(8213));
+        }
+    }
+}
